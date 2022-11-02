785e59f7b30d7927b6104572d423886c83e3fdd4
Petrosyan2001 <petrosyan.petros.2001@gmail.com>
2022-11-02 20:25:03 +0400

    c++

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,7 +3,7 @@ export declare class Compiler {
     public language: Language
     constructor();
 
-   public compile({language, code}: {language: Language, code: string}):{
+   public compile({language, code}: {language: Language, code: string, input?: string}):{
     status: number,
     data: string
    }
